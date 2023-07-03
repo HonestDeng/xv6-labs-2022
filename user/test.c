8b2784fa44ebcc72f271d73cf94e3893e2b99891
Parallels <HonestDeng>
2023-07-03 14:56:11 +0800

    modify some comment

--- user/test.c
+++ user/test.c
@@ -11,8 +11,8 @@ int main(int argc, char *argv[]){
         write(left_p[1], &i, 4);
     }
     int number;
-    while(read(left_p[0], &number, 4) != 0){ // if the pipe have numbers
-        printf("prime %d\n", number);
+    while(read(left_p[0], &number, 4) != 0){ // the pipe have numbers
+        printf("PID: %d, number: %d\n", getpid(), number);
         int right_p[2];
         pipe(right_p);
         int fork_id= fork();
@@ -22,19 +22,26 @@ int main(int argc, char *argv[]){
             int prime = number;
             // read from left and write to right
             while(read(left_p[0], &number, 4) != 0){
+                // printf("dddd\n");
                 if(number % prime != 0){
+                    // if(getpid() == 4){
+                    //     printf("write %d\n", number);
+                    // }
                     write(right_p[1], &number, 4);
                 }
             }
+            // close(right_p[0]);
             close(left_p[0]);
             close(right_p[1]);
-            close(right_p[0]);
             break;
         }
+        // close(left_p[0]);
         close(left_p[1]);
+        // close(right_p[1]);
         left_p[0] = right_p[0];
         left_p[1] = right_p[1];
     }
     wait(0);
+    // printf("PID %d exit\n", getpid());
     exit(0);
 }
